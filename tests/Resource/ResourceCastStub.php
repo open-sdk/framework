32b7e2aac4494e7a15bb5538dc4682524dccc35e
Cedric van Putten <me@bycedric.com>
2017-10-14 22:52:45 +0200

    feature: nested resource and attribute casting

---
+++ tests/Resource/ResourceCastStub.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OpenSdk\Framework\Tests\Resource;
+
+use DateTime;
+use OpenSdk\Framework\Resource\Resource;
+
+class ResourceCastStub extends Resource
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	protected static $casts = [
+		'testEmpty' => '',
+		'testUnkown' => 'biepboep',
+		'testClassNotExists' => 'This\Class\Doesnt\Exists',
+
+		'testBoolean' => 'boolean',
+		'testBooleanFalse' => 'boolean',
+		'testBooleanStringTrue' => 'boolean',
+		'testBooleanStringFalse' => 'boolean',
+		'testBooleanStringOne' => 'boolean',
+		'testBooleanStringZero' => 'boolean',
+
+		'testInteger' => 'integer',
+		'testIntegerStringInteger' => 'integer',
+		'testIntegerStringFloat' => 'integer',
+
+		'testFloat' => 'float',
+		'testFloatString' => 'float',
+		'testFloatInteger' => 'float',
+
+		'testString' => 'string',
+		'testStringInteger' => 'string',
+		'testStringBoolean' => 'string',
+
+		'testClass' => self::class,
+		'testClassDateTime' => DateTime::class,
+
+		'testCallable' => 'strtoupper',
+		'testCallableIntegerValue' => 'intval',
+	];
+}
